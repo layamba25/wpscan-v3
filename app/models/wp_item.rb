6c2a9988733d6ee27db74939078a43950547fe50
erwanlr <erwan.lr@gmail.com>
2017-10-08 20:17:14 +0100

    Ref #28 - Updates the Readmes and Changelog Lists

--- app/models/wp_item.rb
+++ app/models/wp_item.rb
@@ -6,8 +6,8 @@ module WPScan
     include CMSScanner::Target::Platform::PHP
     include CMSScanner::Target::Server::Generic
 
-    READMES    = %w[readme.txt README.txt Readme.txt ReadMe.txt README.TXT readme.TXT].freeze
-    CHANGELOGS = %w[changelog.txt Changelog.txt ChangeLog.txt CHANGELOG.txt].freeze
+    READMES    = %w[readme.txt README.txt README.md readme.md Readme.txt].freeze
+    CHANGELOGS = %w[changelog.txt CHANGELOG.md changelog.md].freeze
 
     attr_reader :uri, :name, :detection_opts, :version_detection_opts, :target, :db_data
 
