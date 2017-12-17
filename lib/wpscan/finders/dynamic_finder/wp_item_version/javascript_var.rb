8c88220a39e8845aa4b2ffac0689790acf969751
erwanlr <erwan.lr@gmail.com>
2017-12-17 20:05:10 +0000

    Adds DFs

--- lib/wpscan/finders/dynamic_finder/wp_item_version/javascript_var.rb
+++ lib/wpscan/finders/dynamic_finder/wp_item_version/javascript_var.rb
@@ -50,7 +50,7 @@ module WPScan
           def version_number_from_match_data(match_data)
             if self.class::VERSION_KEY
               begin
-                json = JSON.parse("{#{match_data[:json]}}")
+                json = JSON.parse("{#{match_data[:json].strip.tr("'", '"')}}")
               rescue JSON::ParserError
                 return
               end
