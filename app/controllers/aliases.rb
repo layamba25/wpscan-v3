f8b33ca7fbbae283bf09010f4d216dfb3326fc4b
erwanlr <erwan.lr@gmail.com>
2018-01-15 20:49:01 +0000

    Ref #47 - Adds a --stealthy cli alias

---
+++ app/controllers/aliases.rb
@@ -0,0 +1,12 @@
+module WPScan
+  module Controller
+    # Controller to add the aliases in the CLI
+    class Aliases < CMSScanner::Controller::Base
+      def cli_options
+        [
+          OptAlias.new(['--stealthy'], alias_for: '--random-user-agent --detection-mode passive')
+        ]
+      end
+    end
+  end
+end
